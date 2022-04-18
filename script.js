be175b5a41d040fe9e91cf2083bcdd28feb294de
danivelarde <danielavelarde4@gmail.com>
2022-04-18 18:49:16 -0400

    changes api github

--- script.js
+++ script.js
@@ -7,39 +7,26 @@ jQuery(document).ready(function($) {
 });
 
 // to display api github
+var usersContainer = document.getElementById('users');
+var fetchButton = document.getElementById('fetch-button');
 
-searchBtnEl = $('#git')
-var blankSpace = $('#test')
-
-function locationLoad() {
-
-  blankSpace.empty()
-  var gitUrl = 'https://api.github.com/users/davelarde/repos'
-
-  console.log("gitUrl")
-
-  fetch(gitUrl, {
-
-  }).then(function (response) {
-    return response.json()
-
-  })
-    .then(function (data) {
-      console.log(data)
-      var gitKey = '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' + data.query.search[0].title
-
-      fetch(gitKey, {
-
-      })
-        .then(function (gitResponse) {
-          return gitResponse.json()
-
-        })
-        .then(function (wikiData) {
-          var pageKey = data.query.search[0].pageid
-          console.log(gitData.query.pages[pageKey].extract)
-          popularEl.append('<h2>' + searchTextEl.val() + '</h2>')
-          popularEl.append('<p class ="text-size" >' + gitData.query.pages[pageKey].extract + '</p>')
-          // descriptionEl.text(wikiData.query.pages[pageKey].extract)
-        })
-    })
+function getApi() {
+var requestUrl ="https://api.github.com/users/davelarde/repos";
+fetch(requestUrl)
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+    for(var i=0; i<data.length; i++){
+        var userName = document.createElement("h3");
+        var userUrl = document.createElement("p");
+        userName.textContent = data[i].owner
+        userUrl.textContent =data[i].deployments_url
+        // usersContainer.append(userName);
+        usersContainer.append(userUrl);
+        
+    }
+});
+}
+fetchButton.addEventListener('click', getApi);
