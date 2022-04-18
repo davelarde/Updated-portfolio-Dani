8fcd61b12d2c80493ffebb48006f7c645bef6f70
danivelarde <danielavelarde4@gmail.com>
2022-04-17 23:06:12 -0400

    layout changes

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
 console.log("hello");
-$('.dropdown-toggle').dropdown('show');
 
-$(".card").hover(function(){
-    $(this).css("background-color", "white");
-    }, function(){
-    $(this).css("background-color", "pink");
-  });
