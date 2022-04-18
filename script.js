a1df773aa11fe8b0adad971c959190c4b3a28c03
danivelarde <danielavelarde4@gmail.com>
2022-04-17 22:03:58 -0400

    layout

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("hello");
+$('.dropdown-toggle').dropdown('show');
+
+$(".card").hover(function(){
+    $(this).css("background-color", "white");
+    }, function(){
+    $(this).css("background-color", "pink");
+  });
